d5ec10f6b1efe36237e28e6092be2c064d3da6f0
Euridd Garbutt <GarbuttE@gobmail.gov.bz>
2022-10-02 18:46:43 -0600

    Cloud Flare

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,8 +2,16 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
+	 interface Locals {}
+	 interface Platform {
+		      env: {
+		          COUNTER: DurableObjectNamespace;
+		      };
+		       context: {
+		           waitUntil(promise: Promise<any>): void;
+		       };
+		       caches: CacheStorage & { default: Cache }
+		   }
 	// interface PrivateEnv {}
 	// interface PublicEnv {}
 	// interface Session {}
